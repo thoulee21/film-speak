604c04feec63c1bf7603739a3d630535fc0be4ce
thoulee <thou_lee@outlook.com>
2024-12-29 19:07:30 +0800

    feat: 更新底部导航栏，添加动画效果并优化图标渲染

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,19 @@
+import { TransitionPresets } from '@react-navigation/bottom-tabs';
+import { CommonActions } from '@react-navigation/native';
 import { Link, Tabs } from 'expo-router';
 import React from 'react';
-import { Pressable } from 'react-native';
-import { Icon } from 'react-native-paper';
+import { Easing, Pressable } from 'react-native';
+import { BottomNavigation, Icon } from 'react-native-paper';
 
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof Icon>['source'];
   color: string;
+  size: number;
 }) {
   return (
     <Icon
-      size={28}
       source={props.name}
       {...props}
     />
@@ -22,30 +24,76 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
+        ...TransitionPresets.ShiftTransition,
         tabBarHideOnKeyboard: true,
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
-        animation: 'fade',
       }}
       backBehavior='none'
+      tabBar={({ navigation, state, descriptors, insets }) => (
+        <BottomNavigation.Bar
+          compact
+          shifting
+          animationEasing={Easing.ease}
+          navigationState={state}
+          safeAreaInsets={insets}
+          onTabPress={({ route, preventDefault }) => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (event.defaultPrevented) {
+              preventDefault();
+            } else {
+              navigation.dispatch({
+                ...CommonActions.navigate(route.name, route.params),
+                target: state.key,
+              });
+            }
+          }}
+          renderIcon={({ route, focused, color }) => {
+            const { options } = descriptors[route.key];
+            if (options.tabBarIcon) {
+              return options.tabBarIcon({ focused, color, size: 24 });
+            }
+
+            return null;
+          }}
+          getLabelText={({ route }) => {
+            const { options } = descriptors[route.key];
+            const label =
+              typeof options.tabBarLabel === 'string'
+                ? options.tabBarLabel
+                : typeof options.title === 'string'
+                  ? options.title
+                  : route.name;
+
+            return label;
+          }}
+        />
+      )}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Video',
-          tabBarIcon: ({ color, focused }) => (
+          tabBarIcon: ({ color, focused, size }) => (
             <TabBarIcon
               name={focused ? 'video' : 'video-outline'}
               color={color}
+              size={size}
             />
           ),
-          headerRight: () => (
+          headerRight: ({ tintColor }) => (
             <Link href="/modal" asChild>
               <Pressable style={{ padding: 10 }}>
                 <Icon
                   source="information-outline"
                   size={25}
+                  color={tintColor}
                 />
               </Pressable>
             </Link>
@@ -56,10 +104,11 @@ export default function TabLayout() {
         name="test"
         options={{
           title: 'Test',
-          tabBarIcon: ({ color }) => (
+          tabBarIcon: ({ color, size }) => (
             <TabBarIcon
               name="test-tube"
               color={color}
+              size={size}
             />
           ),
         }}
