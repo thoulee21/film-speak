5ff5846c506880f34f286d62fcbbb6d594832e9d
thoulee <thou_lee@outlook.com>
2025-01-03 00:15:24 +0800

    feat: 优化历史记录页面，添加文件类型和行数显示，重构组件样式

--- src/app/history.tsx
+++ src/app/history.tsx
@@ -6,7 +6,15 @@ import {
   View,
   type ListRenderItemInfo,
 } from "react-native";
-import { Appbar, Divider, List } from "react-native-paper";
+import {
+  Appbar,
+  Avatar,
+  Chip,
+  Divider,
+  List,
+  Text,
+  useTheme
+} from "react-native-paper";
 
 import { useAppSelector } from "@/src/hooks/redux";
 import {
@@ -15,32 +23,80 @@ import {
 } from "@/src/redux/slices/subtitles";
 
 export default function Subtitles() {
+  const appTheme = useTheme();
   const subtitles = useAppSelector(selectSubtitles);
 
-  const renderItem = ({ item }: ListRenderItemInfo<Subtitle>) => {
+  const renderItem = ({
+    item
+  }: ListRenderItemInfo<Subtitle>) => {
     return (
       <List.Item
         title={item.fileUri}
         titleNumberOfLines={5}
-        description={item.value.length + " captions"}
+        description={({
+          color, fontSize, ellipsizeMode
+        }) => (
+          <View style={{ flexDirection: "row" }}>
+            <Chip
+              icon="subtitles-outline"
+              mode="outlined"
+              textStyle={{ color, fontSize }}
+              style={{ marginTop: 10 }}
+              ellipsizeMode={ellipsizeMode}
+            >
+              {item.value.length} lines
+            </Chip>
+
+            <Chip
+              icon="file-outline"
+              mode="outlined"
+              textStyle={{ color, fontSize }}
+              style={{ marginTop: 10, marginLeft: 10 }}
+              ellipsizeMode={ellipsizeMode}
+            >
+              {item.fileUri.split('.').pop()}
+            </Chip>
+          </View>
+        )}
+        left={({ style, color }) => (
+          <Avatar.Text
+            style={[style, {
+              backgroundColor: appTheme.colors.primaryContainer,
+            }]}
+            labelStyle={{ color }}
+            size={40}
+            label={item.fileUri[0].toUpperCase()}
+          />
+        )}
       />
     );
   };
 
   return (
     <View style={styles.root}>
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+      <StatusBar
+        style={Platform.OS === 'ios' ? 'light' : 'auto'}
+      />
       <Appbar.Header
         statusBarHeight={0}
         elevated
       >
         <Appbar.Content title="History" />
+        <Appbar.Action icon="drag" />
       </Appbar.Header>
 
       <FlatList
         data={subtitles}
         renderItem={renderItem}
         ItemSeparatorComponent={Divider}
+        contentContainerStyle={{ flexGrow: 1 }}
+        ListEmptyComponent={
+          <View style={styles.emptyContainer}>
+            <Text variant="bodyLarge">
+              No subtitles found
+            </Text>
+          </View>
+        }
       />
     </View>
   );
@@ -50,4 +106,9 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
   },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 })
