246753889fee22f60f813b977a9ccc2a26324294
thoulee <thou_lee@outlook.com>
2025-01-03 20:51:05 +0800

    feat: 优化缓存项列表显示，支持文件夹和文件的排序及类型显示

--- src/app/dev/cache.tsx
+++ src/app/dev/cache.tsx
@@ -1,50 +1,61 @@
-import * as FileSystem from 'expo-file-system';
-import { useCallback, useEffect, useState } from 'react';
+import { Directory, File, Paths } from 'expo-file-system/next';
+import { useCallback } from 'react';
 import { FlatList, type ListRenderItemInfo } from 'react-native';
-import { Divider, List, useTheme } from 'react-native-paper';
+import { Caption, Divider, List } from 'react-native-paper';
 
-import type ListLRProps from '@/src/types/paperListItem';
+import { formatDataSize } from '@/src/utils/formatDataSize';
+
+type Content = File | Directory;
 
 export default function CacheScreen() {
-  const appTheme = useTheme();
-  const [cacheItems, setCacheItems] = useState<string[]>([]);
+  // 排序内容。文件夹在前，文件在后，按名称排序
+  const sortContent = useCallback((
+    a: Content, b: Content
+  ) => {
+    if (a instanceof Directory && b instanceof File) {
+      return -1;
+    }
 
-  useEffect(() => {
-    (async () => {
-      if (FileSystem.cacheDirectory) {
-        const files = await FileSystem.readDirectoryAsync(
-          FileSystem.cacheDirectory
-        );
-        //按字母顺序排序，先小写后大写
-        setCacheItems(files.sort(
-          (a, b) => a.localeCompare(b)
-        ));
-      }
-    })();
+    if (a instanceof File && b instanceof Directory) {
+      return 1;
+    }
+
+    return a.name.localeCompare(b.name);
   }, []);
-
-  const renderIcon = useCallback((props: ListLRProps) => (
-    <List.Icon {...props} icon="file-document-outline" />
-  ), []);
 
   const renderItem = useCallback(({
     item
-  }: ListRenderItemInfo<string>) => (
+  }: ListRenderItemInfo<Content>) => (
     <List.Item
-      title={item}
-      titleNumberOfLines={2}
-      left={renderIcon}
-      style={{
-        backgroundColor: item.endsWith('.wav')
-          ? appTheme.colors.tertiaryContainer
-          : undefined
-      }}
+      title={item.name}
+      titleNumberOfLines={3}
+      description={item instanceof File && item.type}
+      left={(props) =>
+        item instanceof File ? (
+          <List.Icon
+            {...props}
+            icon={
+              item.type?.includes('audio')
+                ? 'file-music-outline' : 'file-document-outline'
+            }
+          />
+        ) : (
+          <List.Icon {...props} icon="folder-outline" />
+        )
+      }
+      right={(props) =>
+        item instanceof File && (
+          <Caption {...props}>
+            {formatDataSize(item.size || 0)}
+          </Caption>
+        )
+      }
     />
-  ), [appTheme.colors.tertiaryContainer, renderIcon]);
+  ), []);
 
   return (
     <FlatList
-      data={cacheItems}
+      data={Paths.cache.list().sort(sortContent)}
       renderItem={renderItem}
       ItemSeparatorComponent={Divider}
     />
