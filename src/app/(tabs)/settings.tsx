7f659f0dae402bfab894564caae136f3bf2fe886
thoulee <thou_lee@outlook.com>
2025-01-03 20:53:14 +0800

    feat: 添加分隔符并优化设置界面的主题颜色显示

--- src/app/(tabs)/settings.tsx
+++ src/app/(tabs)/settings.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'expo-router';
 import { useCallback } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { List } from 'react-native-paper';
+import { Divider, List, useTheme } from 'react-native-paper';
 
 import ShowSubtitleSwitchItem from '@/src/components/settings/ShowSubtitleSwitch';
 import { useAppSelector } from '@/src/hooks/redux';
@@ -9,6 +9,7 @@ import { selectDevMode } from '@/src/redux/slices/devMode';
 import type ListLRProps from '@/src/types/paperListItem';
 
 export default function Setting() {
+  const appTheme = useTheme();
   const devModeEnabled = useAppSelector(selectDevMode);
 
   const renderDevIcon = useCallback((props: ListLRProps) => (
@@ -27,9 +28,13 @@ export default function Setting() {
     <View style={styles.container}>
       <List.Section>
         <ShowSubtitleSwitchItem />
+        <Divider />
       </List.Section>
 
-      <List.Section>
+      <List.Section
+        title="General"
+        titleStyle={{ color: appTheme.colors.primary }}
+      >
         {devModeEnabled && (
           <Link href="/dev" asChild>
             <List.Item
