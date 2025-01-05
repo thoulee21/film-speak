23838ab760c5435487b21b758046e15d0cbf8d92
thoulee <thou_lee@outlook.com>
2025-01-05 15:58:18 +0800

    refactor(tabs): 简化底部导航栏布局，移除不必要的组件和样式

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -2,13 +2,8 @@ import {
   TransitionPresets,
   type BottomTabBarProps,
 } from '@react-navigation/bottom-tabs';
-import { router, Tabs } from 'expo-router';
+import { Tabs } from 'expo-router';
 import React, { useCallback } from 'react';
-import { StyleSheet } from 'react-native';
-import HapticFeedback, {
-  HapticFeedbackTypes,
-} from 'react-native-haptic-feedback';
-import { FAB, Portal } from "react-native-paper";
 
 import packageData from '@/package.json';
 import MaterialBottomBar from '@/src/components/tabbar/MaterialBottomBar';
@@ -28,69 +23,49 @@ export default function TabLayout() {
   ), []);
 
   return (
-    <Portal.Host>
-      <Tabs
-        screenOptions={{
-          ...TransitionPresets.ShiftTransition,
-          animation: 'shift',
-          tabBarHideOnKeyboard: true,
-          // Disable the static render of the header on web
-          // to prevent a hydration error in React Navigation v6.
-          headerShown: useClientOnlyValue(false, true),
+    <Tabs
+      screenOptions={{
+        ...TransitionPresets.ShiftTransition,
+        animation: 'shift',
+        tabBarHideOnKeyboard: true,
+        // Disable the static render of the header on web
+        // to prevent a hydration error in React Navigation v6.
+        headerShown: useClientOnlyValue(false, true),
+      }}
+      backBehavior='none'
+      tabBar={renderTabBar}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: packageData.displayName,
+          headerShown: false,
+          tabBarIcon: ({
+            color, focused, size
+          }) => (
+            <TabBarIcon
+              name={focused ? 'video' : 'video-outline'}
+              color={color}
+              size={size}
+            />
+          ),
         }}
-        backBehavior='none'
-        tabBar={renderTabBar}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: packageData.displayName,
-            headerShown: false,
-            tabBarIcon: ({ color, focused, size }) => (
-              <TabBarIcon
-                name={focused ? 'video' : 'video-outline'}
-                color={color}
-                size={size}
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="settings"
-          options={{
-            title: 'Settings',
-            tabBarIcon: ({ color, size, focused }) => (
-              <TabBarIcon
-                name={focused ? 'cog' : 'cog-outline'}
-                color={color}
-                size={size}
-              />
-            ),
-          }}
-        />
-      </Tabs>
-
-      <Portal>
-        <FAB
-          icon="subtitles-outline"
-          style={styles.fab}
-          onPress={() => {
-            HapticFeedback.trigger(
-              HapticFeedbackTypes.effectDoubleClick,
-            );
-            router.push('/subtitles');
-          }}
-        />
-      </Portal>
-    </Portal.Host>
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({
+            color, size, focused
+          }) => (
+            <TabBarIcon
+              name={focused ? 'cog' : 'cog-outline'}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
-
-const styles = StyleSheet.create({
-  fab: {
-    position: 'absolute',
-    margin: 16,
-    right: 0,
-    bottom: 80
-  }
-})
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import * as Crypto from 'expo-crypto';
 import * as FileSystem from 'expo-file-system';
+import { router } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import {
   FFmpegKit,
@@ -16,8 +17,11 @@ import {
   ToastAndroid,
   View,
 } from 'react-native';
+import HapticFeedback, {
+  HapticFeedbackTypes,
+} from 'react-native-haptic-feedback';
 import Video from 'react-native-media-console';
-import { useTheme } from 'react-native-paper';
+import { FAB, Portal, useTheme } from 'react-native-paper';
 import {
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
@@ -86,88 +90,103 @@ export default function VideoScreen() {
   }, [handleShare]);
 
   return (
-    <View style={[
-      styles.root,
-      { paddingTop: insets.top }
-    ]}>
-      <View style={styles.videoContainer}>
-        <Video
-          videoRef={player}
-          source={{ uri: source }}
-          seekColor={appTheme.colors.secondary}
-          shutterColor={appTheme.colors.secondaryContainer}
-          showNotificationControls
-          automaticallyWaitsToMinimizeStalling
-          onLoad={() => { player.current?.pause(); }}
-          onProgress={({
-            currentTime
-          }) => {
-            if (!clip) { return; }
+    <Portal.Host>
+      <View style={[
+        styles.root,
+        { paddingTop: insets.top }
+      ]}>
+        <View style={styles.videoContainer}>
+          <Video
+            videoRef={player}
+            source={{ uri: source }}
+            seekColor={appTheme.colors.secondary}
+            shutterColor={appTheme.colors.secondaryContainer}
+            showNotificationControls
+            automaticallyWaitsToMinimizeStalling
+            onLoad={() => { player.current?.pause(); }}
+            onProgress={({
+              currentTime
+            }) => {
+              if (!clip) { return; }
 
-            //在片段的开始和结束之间循环播放
-            if (
-              currentTime >= clip.startSeconds &&
-              currentTime <= clip.endSeconds
-            ) { return; }
+              //在片段的开始和结束之间循环播放
+              if (
+                currentTime >= clip.startSeconds &&
+                currentTime <= clip.endSeconds
+              ) { return; }
 
-            player.current?.seek(clip.startSeconds);
-          }}
-          showDuration
-          showTimeRemaining
-          disableBack
-          disableFullscreen
-          // fullscreenOrientation='landscape'
-          // fullscreenAutorotate
-          // isFullscreen={fullscreen}
-          // onEnterFullscreen={async () => {
-          //   setFullscreen(true);
-          //   navigation.setOptions({ headerShown: false })
+              player.current?.seek(clip.startSeconds);
+            }}
+            showDuration
+            showTimeRemaining
+            disableBack
+            disableFullscreen
+            // fullscreenOrientation='landscape'
+            // fullscreenAutorotate
+            // isFullscreen={fullscreen}
+            // onEnterFullscreen={async () => {
+            //   setFullscreen(true);
+            //   navigation.setOptions({ headerShown: false })
 
-          //   await ScreenOrientation.lockAsync(
-          //     OrientationLock.LANDSCAPE_RIGHT
-          //   );
-          // }}
-          // onExitFullscreen={async () => {
-          //   await ScreenOrientation.lockAsync(
-          //     OrientationLock.PORTRAIT_UP
-          //   );
+            //   await ScreenOrientation.lockAsync(
+            //     OrientationLock.LANDSCAPE_RIGHT
+            //   );
+            // }}
+            // onExitFullscreen={async () => {
+            //   await ScreenOrientation.lockAsync(
+            //     OrientationLock.PORTRAIT_UP
+            //   );
 
-          //   setFullscreen(false);
-          //   navigation.setOptions({ headerShown: true });
-          // }}
-          onError={({ error }) => {
-            ToastAndroid.showWithGravity(
-              'Video error: ' + error.errorException,
-              ToastAndroid.LONG,
-              ToastAndroid.BOTTOM
-            );
-            console.error('Video error:', error);
+            //   setFullscreen(false);
+            //   navigation.setOptions({ headerShown: true });
+            // }}
+            onError={({ error }) => {
+              ToastAndroid.showWithGravity(
+                'Video error: ' + error.errorException,
+                ToastAndroid.LONG,
+                ToastAndroid.BOTTOM
+              );
+              console.error('Video error:', error);
 
-            ToastAndroid.showWithGravity(
-              'Resetting video source ...',
-              ToastAndroid.LONG,
-              ToastAndroid.BOTTOM
-            );
-            dispatch(resetVideoSource());
-          }}
-          onLayout={SplashScreen.hideAsync}
-          disableOverlay
-          paused
-          // if volume is unset(-1), play at original volume
-          volume={volume === -1 ? (1 / volumeFactor) : volume}
-          onVolumeChange={({ volume }) => {
-            dispatch(setVolume(volume));
+              ToastAndroid.showWithGravity(
+                'Resetting video source ...',
+                ToastAndroid.LONG,
+                ToastAndroid.BOTTOM
+              );
+              dispatch(resetVideoSource());
+            }}
+            onLayout={SplashScreen.hideAsync}
+            disableOverlay
+            paused
+            // if volume is unset(-1), play at original volume
+            volume={volume === -1 ? (1 / volumeFactor) : volume}
+            onVolumeChange={({ volume }) => {
+              dispatch(setVolume(volume));
+            }}
+          />
+        </View>
+
+        <Subtitle
+          onItemPress={(item) => {
+            setClip(item);
+            player.current?.resume();
           }}
         />
-      </View>
 
-      <Subtitle
-        onItemPress={(item) => {
-          setClip(item);
-          player.current?.resume();
-        }}
-      />
-    </View>
+        <Portal>
+          <FAB
+            icon="subtitles-outline"
+            style={styles.fab}
+            onPress={() => {
+              HapticFeedback.trigger(
+                HapticFeedbackTypes.effectDoubleClick,
+              );
+              router.push('/subtitles');
+            }}
+          />
+        </Portal>
+      </View>
+    </Portal.Host>
   );
 }
 
@@ -179,4 +198,10 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "35%",
   },
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 80
+  }
 });
