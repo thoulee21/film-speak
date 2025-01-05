943263ee987434f27f9962fc6b9bd62779eb1092
thoulee <thou_lee@outlook.com>
2025-01-05 17:48:51 +0800

    feat(subtitles): 重构字幕组件，提取字幕项为独立组件，优化代码结构

--- src/app/subtitles.tsx
+++ src/app/subtitles.tsx
@@ -1,129 +1,32 @@
-import Clipboard from "@react-native-clipboard/clipboard";
-import * as Crypto from "expo-crypto";
-import { File, Paths } from "expo-file-system/next";
-import { router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useCallback } from "react";
 import {
   Platform,
   StyleSheet,
-  ToastAndroid,
   View,
-  type ListRenderItemInfo,
+  type ListRenderItemInfo
 } from "react-native";
-import HapticFeedback, {
-  HapticFeedbackTypes,
-} from "react-native-haptic-feedback";
-import {
-  Appbar,
-  Caption,
-  Divider,
-  IconButton,
-  List,
-  Text,
-  useTheme
-} from "react-native-paper";
+import { Appbar } from "react-native-paper";
 import Reanimated, {
   LinearTransition,
 } from 'react-native-reanimated';
 
 import LottieAnimation from "@/src/components/LottieAnimation";
+import SubtitleItem from "@/src/components/subtitles/item";
+import { useAppSelector } from "@/src/hooks/redux";
 import {
-  useAppDispatch,
-  useAppSelector
-} from "@/src/hooks/redux";
-import {
-  removeSubtitle,
   selectSubtitles,
-  type Subtitle,
+  type Subtitle
 } from "@/src/redux/slices/subtitles";
-import {
-  selectVideoSource,
-  setVideoSource
-} from "@/src/redux/slices/videoSource";
 
 export default function Subtitles() {
-  const dispatch = useAppDispatch();
-  const appTheme = useTheme();
-
   const subtitles = useAppSelector(selectSubtitles);
-  const videoSource = useAppSelector(selectVideoSource);
 
-  const renderItem = ({
+  const renderItem = useCallback(({
     item
   }: ListRenderItemInfo<Subtitle>) => (
-    <List.Item
-      title={(props) => (
-        <View style={styles.titleSection}>
-          <Text {...props}>
-            {new Date(item.createAt).toLocaleString()}
-          </Text>
-          <Caption>
-            {item.value.length} line(s)
-          </Caption>
-        </View>
-      )}
-      description={item.fileUri}
-      style={{
-        backgroundColor: videoSource === item.fileUri
-          ? appTheme.colors.secondaryContainer
-          : undefined
-      }}
-      onPress={() => {
-        HapticFeedback.trigger(HapticFeedbackTypes.effectClick);
-
-        if (videoSource !== item.fileUri) {
-          dispatch(setVideoSource(item.fileUri));
-          router.back();
-        }
-      }}
-      onLongPress={() => {
-        HapticFeedback.trigger(
-          HapticFeedbackTypes.effectHeavyClick
-        );
-        Clipboard.setString(item.fileUri);
-
-        ToastAndroid.show(
-          "Copied to clipboard",
-          ToastAndroid.SHORT
-        )
-      }}
-      left={(props) => (
-        <List.Image
-          variant="video"
-          source={{ uri: item.coverUri }}
-          {...props}
-        />
-      )}
-      right={(props) => (
-        <IconButton
-          {...props}
-          icon="delete-forever-outline"
-          iconColor={appTheme.colors.error}
-          disabled={videoSource === item.fileUri}
-          onPress={async () => {
-            HapticFeedback.trigger(
-              HapticFeedbackTypes.effectClick
-            );
-            const fileHash = await Crypto.digestStringAsync(
-              Crypto.CryptoDigestAlgorithm.SHA256,
-              item.fileUri
-            );
-            const file2delete = new File(
-              Paths.cache,
-              `${fileHash.slice(0, 6)}.wav`
-            );
-
-            file2delete.exists && file2delete.delete();
-            dispatch(removeSubtitle(item.fileUri));
-            ToastAndroid.show(
-              "Subtitle removed",
-              ToastAndroid.SHORT
-            );
-          }}
-        />
-      )}
-    />
-  );
+    <SubtitleItem item={item} />
+  ), []);
 
   return (
     <View style={styles.root}>
@@ -141,8 +44,7 @@ export default function Subtitles() {
       <Reanimated.FlatList
         data={subtitles}
         renderItem={renderItem}
-        ItemSeparatorComponent={Divider}
-        contentContainerStyle={{ flexGrow: 1 }}
+        contentContainerStyle={styles.items}
         ListEmptyComponent={
           <LottieAnimation
             animation="teapot"
@@ -159,8 +61,8 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
   },
-  titleSection: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+  items: {
+    flexGrow: 1,
+    marginHorizontal: 10,
   }
 })
---
+++ src/components/subtitles/item.tsx
@@ -0,0 +1,119 @@
+import Clipboard from "@react-native-clipboard/clipboard";
+import * as Crypto from "expo-crypto";
+import { File, Paths } from "expo-file-system/next";
+import { useCallback } from "react";
+import { StyleSheet, ToastAndroid } from "react-native";
+import HapticFeedback, { HapticFeedbackTypes } from "react-native-haptic-feedback";
+import { Avatar, Button, Caption, Card, Text, useTheme } from "react-native-paper";
+
+import { useAppDispatch, useAppSelector } from "@/src/hooks/redux";
+import { removeSubtitle, type Subtitle } from "@/src/redux/slices/subtitles";
+import { selectVideoSource, setVideoSource } from "@/src/redux/slices/videoSource";
+
+
+export default function SubtitleItem({ item }: { item: Subtitle }) {
+  const dispatch = useAppDispatch();
+  const appTheme = useTheme();
+
+  const videoSource = useAppSelector(selectVideoSource);
+  const selected = videoSource === item.fileUri;
+
+  const selectSubtitle = useCallback(() => {
+    HapticFeedback.trigger(HapticFeedbackTypes.effectClick);
+
+    if (videoSource !== item.fileUri) {
+      dispatch(setVideoSource(item.fileUri));
+    }
+  }, [dispatch, item.fileUri, videoSource]);
+
+  const copyToClipboard = useCallback(() => {
+    HapticFeedback.trigger(
+      HapticFeedbackTypes.effectHeavyClick
+    );
+    Clipboard.setString(item.fileUri);
+
+    ToastAndroid.show(
+      "File URI copied",
+      ToastAndroid.SHORT
+    )
+  }, [item.fileUri]);
+
+  const performRemove = useCallback(async () => {
+    HapticFeedback.trigger(
+      HapticFeedbackTypes.effectClick
+    );
+    const fileHash = await Crypto.digestStringAsync(
+      Crypto.CryptoDigestAlgorithm.SHA256,
+      item.fileUri
+    );
+    const wavFile = new File(
+      Paths.cache,
+      `${fileHash.slice(0, 6)}.wav`
+    );
+
+    wavFile.exists && wavFile.delete();
+    dispatch(removeSubtitle(item.fileUri));
+
+    ToastAndroid.show(
+      "Subtitle removed",
+      ToastAndroid.SHORT
+    );
+  }, [dispatch, item.fileUri]);
+
+  return (
+    <Card
+      onPress={selectSubtitle}
+      onLongPress={copyToClipboard}
+      mode={selected ? "elevated" : "contained"}
+      style={[styles.card, {
+        backgroundColor: selected
+          ? appTheme.colors.primaryContainer
+          : appTheme.colors.surfaceVariant,
+      }]}
+    >
+      <Card.Cover source={{ uri: item.coverUri }} />
+      <Card.Title
+        title={new Date(item.createAt).toLocaleString()}
+        subtitle={`${item.value.length} line(s)`}
+        left={({ size }) => (
+          <Avatar.Icon
+            size={size}
+            icon={selected ? "check" : "subtitles-outline"}
+            style={{
+              backgroundColor: selected
+                ? appTheme.colors.primary
+                : appTheme.colors.backdrop,
+            }}
+          />
+        )}
+        right={({ size }) => (
+          <Caption style={{
+            marginRight: 16,
+            fontSize: size - 6,
+          }}>
+            {item.fileUri.split(".").pop()?.toLocaleUpperCase()}
+          </Caption>
+        )}
+      />
+      <Card.Content>
+        <Text>{item.fileUri}</Text>
+      </Card.Content>
+
+      <Card.Actions>
+        <Button
+          icon="delete-outline"
+          onPress={performRemove}
+          disabled={selected}
+        >
+          Delete
+        </Button>
+      </Card.Actions>
+    </Card>
+  )
+}
+
+const styles = StyleSheet.create({
+  card: {
+    marginVertical: 5,
+  }
+})
