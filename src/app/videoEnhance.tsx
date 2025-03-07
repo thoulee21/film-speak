a56706f9729a56bc9518c08dffb14770fb0ebfbb
thoulee <thou_lee@outlook.com>
2025-03-08 00:31:18 +0800

    feat(videoEnhance): 添加VolFactorSlider组件并更新国际化文本以增强视频因子控制功能

--- src/app/videoEnhance.tsx
+++ src/app/videoEnhance.tsx
@@ -1,138 +1,24 @@
-import Slider from "@react-native-community/slider";
-import { useCallback } from "react";
 import { useTranslation } from "react-i18next";
-import {
-  ScrollView,
-  StyleSheet,
-  View,
-} from "react-native";
-import {
-  Banner,
-  Caption,
-  Divider,
-  IconButton,
-  List,
-  Text,
-  useTheme,
-} from "react-native-paper";
+import { ScrollView, View } from "react-native";
+import { Banner, Divider } from "react-native-paper";
 
+import VolFactorSlider from "@/src/components/videoEnhance/VolFactorSlider";
 import VolumeSlider from "@/src/components/videoEnhance/VolumeSlider";
-import {
-  useAppDispatch,
-  useAppSelector,
-} from "@/src/hooks/redux";
-import {
-  initialState as initialVolumeFactorState,
-  resetVolumeFactor,
-  selectVolumeFactor,
-  setVolumeFactor,
-} from "@/src/store/slices/volumeFactor";
-import type ListLRProps from "@/src/types/paperListItem";
-
-const MIN = 1.0;
-const MAX = 20.0;
-const STEP = 1.0;
 
 export default function VideoEnhanceScreen() {
-  const dispatch = useAppDispatch();
   const { t } = useTranslation();
-
-  const appTheme = useTheme();
-  const volumeFactor = useAppSelector(selectVolumeFactor);
-
-  const renderVolumeFactorIcon = useCallback((
-    props: ListLRProps
-  ) => (
-    <List.Icon {...props} icon="volume-high" />
-  ), []);
 
   return (
     <View>
       <Banner visible>
-        The changes you make here will not affect the video processed earlier. To apply the changes, you need to reprocess the video.
+        {t('videoEnhance.banner')}
       </Banner>
 
       <ScrollView>
-        <List.Section>
-          <List.Item
-            title={({
-              color, ellipsizeMode, fontSize, selectable
-            }) => (
-              <View style={styles.title}>
-                <Text
-                  style={{ color, fontSize }}
-                  ellipsizeMode={ellipsizeMode}
-                  selectable={selectable}
-                >
-                  {t('videoEnhance.factors')}
-                </Text>
-
-                <Text style={{ fontSize }}>
-                  {volumeFactor.toFixed(1)}x
-                </Text>
-              </View>
-            )}
-            description={`Increase or decrease the video factor, default is ${initialVolumeFactorState.value.toFixed(1)}`}
-            left={renderVolumeFactorIcon}
-            right={({ style, color }) => (
-              <IconButton
-                icon="refresh"
-                style={style}
-                iconColor={color}
-                onPress={() => dispatch(resetVolumeFactor())}
-              />
-            )}
-          />
-
-          <Slider
-            minimumValue={MIN}
-            maximumValue={MAX}
-            StepMarker={() => (
-              <View style={[
-                styles.stepMaker,
-                { backgroundColor: appTheme.colors.primary }
-              ]} />
-            )}
-            step={STEP}
-            value={volumeFactor}
-            onSlidingComplete={(value) => {
-              dispatch(setVolumeFactor(value));
-            }}
-            style={styles.slider}
-            thumbTintColor={appTheme.colors.primary}
-            minimumTrackTintColor={appTheme.colors.onPrimaryContainer}
-            maximumTrackTintColor={appTheme.colors.outline}
-          />
-
-          <View style={styles.sliderCaption}>
-            <Caption>{MIN.toFixed(1)}</Caption>
-            <Caption>{MAX.toFixed(1)}</Caption>
-          </View>
-        </List.Section>
+        <VolFactorSlider />
         <Divider />
-
         <VolumeSlider />
       </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  title: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  slider: {
-    marginHorizontal: 6,
-  },
-  stepMaker: {
-    width: 2,
-    height: 2,
-    borderRadius: 5,
-  },
-  sliderCaption: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginHorizontal: 20,
-  }
-})
---
+++ src/components/videoEnhance/VolFactorSlider.tsx
@@ -0,0 +1,110 @@
+import React, { useCallback } from "react";
+import { useTranslation } from "react-i18next";
+import { StyleSheet, View } from "react-native";
+import { Caption, IconButton, List, Text, useTheme } from "react-native-paper";
+
+import { useAppDispatch, useAppSelector } from "@/src/hooks/redux";
+import {
+  resetVolumeFactor,
+  selectVolumeFactor,
+  setVolumeFactor,
+} from "@/src/store/slices/volumeFactor";
+import type ListLRProps from "@/src/types/paperListItem";
+import Slider from "@react-native-community/slider";
+
+const MIN = 1.0;
+const MAX = 20.0;
+const STEP = 1.0;
+
+export default function VolFactorSlider() {
+  const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+
+  const appTheme = useTheme();
+  const volumeFactor = useAppSelector(selectVolumeFactor);
+
+  const renderVolumeFactorIcon = useCallback((
+    props: ListLRProps
+  ) => (
+    <List.Icon {...props} icon="volume-high" />
+  ), []);
+
+  return (
+    <List.Section>
+      <List.Item
+        title={({
+          color, ellipsizeMode, fontSize, selectable
+        }) => (
+          <View style={styles.title}>
+            <Text
+              style={{ color, fontSize }}
+              ellipsizeMode={ellipsizeMode}
+              selectable={selectable}
+            >
+              {t('videoEnhance.factors')}
+            </Text>
+
+            <Text style={{ fontSize }}>
+              {volumeFactor.toFixed(1)}x
+            </Text>
+          </View>
+        )}
+        description={t('videoEnhance.volFactor.description')}
+        left={renderVolumeFactorIcon}
+        right={({ style, color }) => (
+          <IconButton
+            icon="refresh"
+            style={style}
+            iconColor={color}
+            onPress={() => dispatch(resetVolumeFactor())}
+          />
+        )}
+      />
+
+      <Slider
+        minimumValue={MIN}
+        maximumValue={MAX}
+        StepMarker={() => (
+          <View style={[
+            styles.stepMaker,
+            { backgroundColor: appTheme.colors.primary }
+          ]} />
+        )}
+        step={STEP}
+        value={volumeFactor}
+        onSlidingComplete={(value) => {
+          dispatch(setVolumeFactor(value));
+        }}
+        style={styles.slider}
+        thumbTintColor={appTheme.colors.primary}
+        minimumTrackTintColor={appTheme.colors.onPrimaryContainer}
+        maximumTrackTintColor={appTheme.colors.outline}
+      />
+
+      <View style={styles.sliderCaption}>
+        <Caption>{MIN.toFixed(1)}</Caption>
+        <Caption>{MAX.toFixed(1)}</Caption>
+      </View>
+    </List.Section>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  slider: {
+    marginHorizontal: 6,
+  },
+  stepMaker: {
+    width: 2,
+    height: 2,
+    borderRadius: 5,
+  },
+  sliderCaption: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 20,
+  }
+})
