337c4c371f8d2235fc858b01c562cf7e14b06229
thoulee <thou_lee@outlook.com>
2025-04-03 20:48:15 +0800

    feat: add react-native-json-tree for enhanced JSON viewing capabilities

--- src/components/dev/appdata/JsonViewer.tsx
+++ src/components/dev/appdata/JsonViewer.tsx
@@ -1,33 +1,30 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { Text, useTheme } from 'react-native-paper';
+import { Base16Theme, google } from 'base16';
+import React, { useMemo } from 'react';
+import { ScrollView } from 'react-native';
+import JsonView from 'react-native-json-tree';
+import { useTheme } from 'react-native-paper';
 
 export const JSONViewer = ({ data }: { data: any }) => {
   const appTheme = useTheme();
+
   const showingData = data || { info: 'No data to show' };
+  const dynamicTheme: Base16Theme = useMemo(() => ({
+    ...google,
+    base00: appTheme.colors.surfaceVariant,
+  }), [appTheme]);
 
   return (
     <ScrollView
       horizontal
-      style={[
-        styles.jsonView,
-        { borderRadius: appTheme.roundness }
-      ]}
+      style={{ borderRadius: appTheme.roundness * 3 }}
     >
-      <Text style={styles.json}>
-        {JSON.stringify(showingData, null, 2)}
-      </Text>
+      <JsonView
+        data={showingData}
+        theme={dynamicTheme}
+        invertTheme={false}
+        hideRoot
+        shouldExpandNode={() => true}
+      />
     </ScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  json: {
-    margin: 10,
-  },
-  jsonView: {
-    marginBottom: 10,
-    height: 'auto',
-    width: '100%',
-  },
-});
